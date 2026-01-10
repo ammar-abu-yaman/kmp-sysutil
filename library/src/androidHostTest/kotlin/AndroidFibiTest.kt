e42d91a86632f1d7216b83af22d05162d9f3fa5b
ammar <ammar.abu.yaman@gmail.com>
2026-01-10 20:06:20 +0300

    initial commit

---
+++ library/src/androidHostTest/kotlin/AndroidFibiTest.kt
@@ -0,0 +1,12 @@
+package com.ammarymn.kmp.sysinfo
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AndroidFibiTest {
+
+    @Test
+    fun testThirdElement() {
+        assertEquals(3, generateFibi().take(3).last())
+    }
+}
